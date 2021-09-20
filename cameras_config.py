401b64f2daa4f583c689993445e898d0af1f4bf0
Mebarek Saf <30500856+Mebareksaf@users.noreply.github.com>
2021-09-20 23:33:51 +0200

    Create cameras_config.py

---
+++ cameras_config.py
@@ -0,0 +1,5 @@
+camera_links = [
+    "rtsp://admin:admin123@192.168.1.108:554/cam/realmonitor?channel=1&subtype=2", #&proto=Onvif",
+    #0,
+     #"rtsp://admin:admin123@192.168.1.108:554/cam/realmonitor?channel=1&subtype=1"
+     ]
